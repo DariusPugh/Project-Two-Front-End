53e6a4bca517854d9ba4f83d73f5abf279bf725e
Darius <dpugh3@aum.edu>
2018-06-19 16:37:14 -0400

    added several components

--- review-api/src/App.tsx
+++ review-api/src/App.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Col, Container, Row } from 'reactstrap';
 import './App.css';
-import { Sidebar } from './components/sidebar/sidebar.component'
+import { CreateItemComponent } from './components/create-item/create-item.component';
+import { RegisterComponent } from './components/register/register.component';
+import { Sidebar } from './components/sidebar/sidebar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { TestComponent } from './components/test/test.component';
 import { TestComponent2 } from './components/test/test2.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { RegisterComponent } from './components/register/register.component';
-import { CreateItemComponent } from './components/create-item/create-item.component';
 
 class App extends React.Component {
   public render() {
