a026fd3242df9bb8d1cc4d78018ea36de4ce4bea
Darius <dpugh3@aum.edu>
2018-06-21 12:49:17 -0400

    added sign-in functionality with some errors

--- review-api/src/reducers/index.ts
+++ review-api/src/reducers/index.ts
@@ -1,25 +1,39 @@
 import { combineReducers } from "redux";
-import {sidebarReducer} from "../reducers/sidebar.reducer"
+import { sidebarReducer } from "../reducers/sidebar.reducer"
+import { signInReducer } from "./sign-in.reducer";
+import { CognitoUser } from "amazon-cognito-identity-js";
+import { cognitoUserReducer } from "./cognito-user.reducer";
 
 
-// export interface ISignIn {
-//   username: string,
-//   password: string,
-//   errorMessage: string
-// }
+export interface ICognitoUser { 
+  user: CognitoUser | null
+}
+
+export interface ISignIn {
+  firstSignIn: {
+    code: string,
+    isFirstSignIn: boolean,
+    password: string,
+    passwordConfirmation: string
+  },
+  username: string,
+  password: string,
+  errorMessage: string
+}
 
 export interface ISidebar{
     input: string,
     items: any[],
-
 }
 
 
 export interface IState {
-//   signIn: ISignIn,
-  sidebar: ISidebar,
+  cognitoUser: ICognitoUser,
+  signIn: ISignIn,
+  sidebar: ISidebar
 };
-
 export const state = combineReducers<IState>({
-  sidebar: sidebarReducer
+  cognitoUser: cognitoUserReducer,
+  sidebar: sidebarReducer,
+  signIn: signInReducer
 });
