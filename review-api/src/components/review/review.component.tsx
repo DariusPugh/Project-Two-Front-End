3d86b72e7ab747782f84f989903bea3b7e787cb2
Medlock <themedlocksp@gmail.com>
2018-06-22 08:55:48 -0500

    Completed comment functionality. TODO: auth control of posting functionality

--- review-api/src/components/review/review.component.tsx
+++ review-api/src/components/review/review.component.tsx
@@ -5,6 +5,7 @@ export class ReviewComponent extends React.Component<any, any> {
     constructor(props:any) {
         super(props);
         this.state = {
+            comment: '',
             review: {
                 comments: [],
             },
@@ -13,7 +14,7 @@ export class ReviewComponent extends React.Component<any, any> {
 
     public componentDidMount() {
         let rid;
-        if (this.props.category.category && this.props.item.title && this.props.review.reviewID) {
+        if (this.props.review.reviewID) {
             rid = this.props.review.reviewID;
         } else {
             const splitPath = this.props.location.pathname.split('/');
@@ -43,7 +44,53 @@ export class ReviewComponent extends React.Component<any, any> {
                         </div>
                     );
                 })}
+                <div className="form-group">
+                    <div>Comment:</div>
+                    <textarea name="body" className="form-control" rows={3} id="comment" placeholder="Post a comment on this review." onChange={this.inputChange} value={this.state.comment}/>
+                </div>
+                <button className="btn btn-default text-right" role="button" onClick={this.submit} type="button">Submit</button>
+            
             </div>
         );
+    }
+
+    private submit = () => {
+        if (this.state.comment) {
+            let rid;
+            if (this.props.review.reviewID) {
+                rid = this.props.review.reviewID;
+            } else {
+                const splitPath = this.props.location.pathname.split('/');
+                rid = splitPath[splitPath.length-1];
+            }
+            const com = {
+                message: this.state.comment,
+                username: /* getusername from logged in state */ 'Dynamo',
+            }
+            netService.postData(`/review/${rid}`, com)
+                .then((data) => {
+                    const comments = this.state.review.comments;
+                    comments.push({
+                        message: this.state.comment,
+                        username: /* get username from logged in state */ 'Dynamo',
+                    });
+                    const rev = this.state.review;
+                    rev.comments = comments;
+                    this.setState({
+                        ...this.state,
+                        comment: '',
+                        review: rev,
+                    });
+                }).catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+
+    private inputChange = (e:any) => {
+        this.setState({
+            ...this.state,
+            [e.target.id]: e.target.value
+        });
     }
 }
