60d3334582988833ab8905730d6fd2236833fc3f
David S <davidserna2005@gmail.com>
2018-06-27 08:02:17 -0500

    changes to display item components

--- review-api/src/components/display-items-name/display-item.component.tsx
+++ review-api/src/components/display-items-name/display-item.component.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-
-
+import { ListGroup , ListGroupItem} from 'reactstrap';
+// , Badge
 export class DisplayItemComponent extends React.Component<any, any> {
 
   constructor(props: any) {
@@ -17,25 +17,35 @@ export class DisplayItemComponent extends React.Component<any, any> {
   public render() {
     return (
       <div>
-        {this.props.sidebar.items.map((item:any,i:number) =>{
-          return(
-            <div className="container-fluid" key={"container" + i}>
-                <div className = "row" onClick={(e)=> this.navigateToItem(e,item)}>
-                  {item.title}
+        <ListGroup>
+          {this.props.sidebar.items.map((item:any,i:number) =>{
+            return(
+              <ListGroupItem key={"list"+i} className="list-group-item d-flex justify-content-between align-items-center list-group-item list-group-item-dark">
+                <div className="container-fluid" key={"container" + i}>
+                  <div className="row no-pad">
+                    <div className="col-2">
+                      <span><img src="https://images.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg?auto=compress&cs=tinysrgb&h=350" alt=""/></span>
+                    </div>
+                    <div className="col-sm-10">
+                      <div id="display-list-title" className = "row" onClick={(e)=> this.navigateToItem(e,item)}>
+                        <strong>{item.title}</strong>
+                      </div>
+                      <div className = "row">
+                        Category: {item.category}
+                      </div>
+                      <div className = "row" >
+                        Description: <em>{item.description}</em>
+                      </div>
+                      <div className = "row">
+                        Average Score: {item.averageScore}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className = "row">
-                  Category: {item.category}
-                </div>
-                <div className = "row" >
-                  Description: {item.description}
-                </div>
-                <div className = "row">
-                  Average Score: {item.averageScore}
-                </div>
-                <hr/>
-            </div>
-          )
-        })}
+              </ListGroupItem>
+            )
+          })}
+        </ListGroup>
       </div>
     );
   }
